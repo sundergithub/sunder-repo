a423a769b25d8b59b2467da15cdbc9d61b1a20dd
sundergithub <sundercando@gmail.com>
2016-10-09 14:58:11 -0400

    Sample project commit

---
+++ src/main/java/com/javacodegeeks/App.java
@@ -0,0 +1,20 @@
+package com.javacodegeeks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String reverseString(String src){
+    	return new StringBuilder(src).reverse().toString();
+    }
+	
+	public static void main( String[] args )
+    {
+        final String str ="Hello!";
+		App appObject = new App();
+        System.out.println( "Thre reverse strinf of\""+ str + "\" is \""+ 
+		                      appObject.reverseString(str) + "\"" );
+    }
+}
